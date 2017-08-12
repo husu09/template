3b6f4a9d662af0c577179155459632d1815fb3c7
husu09 <husu09@qq.com>
2017-08-12 18:05:37 +0800

    t

---
+++ src/main/java/com/su/concurrency/MasterWokerDemo.java
@@ -0,0 +1,118 @@
+package com.su.concurrency;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * @author husu
+ * MasterWoker 模式
+ * Master 分配任务，汇总计算结果
+ */
+public class MasterWokerDemo {
+	
+	/**
+	 * @author husu
+	 * 任务
+	 */
+	static public class Task {
+		private int id;
+		private int price;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public int getPrice() {
+			return price;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		
+		
+	}
+	/**
+	 * @author husu
+	 * 控制
+	 */
+	static public class Master {
+		// 1 工作者
+		private HashMap<Integer, Thread> wokerMap = new HashMap<>();
+		// 2 任务
+		private ConcurrentLinkedQueue<Task> tasks = new ConcurrentLinkedQueue<>();
+		// 3运行结果
+		private ConcurrentHashMap<Integer, Integer> result = new ConcurrentHashMap<>();
+		
+		public Master(Woker woker, int num) {
+			woker.setTasks(tasks);
+			woker.setResult(result);
+			for (int i = 0; i < num; i++) {
+				wokerMap.put(i, new Thread(woker));
+			}
+		}
+		
+		// 4开始执行
+		public void execute() {
+			for(Entry<Integer, Thread> e : wokerMap.entrySet()) {
+				e.getValue().start();
+			}
+		}
+		
+		// 5是否完成
+		public boolean isComplete() {
+			for(Entry<Integer, Thread> e : wokerMap.entrySet()) {
+				if (e.getValue().getState() != Thread.State.TERMINATED) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		// 6获取结果
+		public int getResult() {
+			int res = 0;
+			for(Entry<Integer, Integer> e : result.entrySet()) {
+				res += e.getValue();
+			}
+			return res;
+		}
+		
+	}
+	
+	/**
+	 * @author husu
+	 * 工作者
+	 */
+	static public class Woker implements Runnable {
+		// 2 任务
+		private ConcurrentLinkedQueue<Task> tasks;
+		// 3运行结果
+		private ConcurrentHashMap<Integer, Integer> result;
+		
+		public void setTasks(ConcurrentLinkedQueue<Task> tasks) {
+			this.tasks = tasks;
+		}
+
+		public void setResult(ConcurrentHashMap<Integer, Integer> result) {
+			this.result = result;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				Task t = tasks.poll();
+				if (t == null) break;
+				try {
+					//任务操作
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				result.put(t.getId(), t.getPrice());
+			}
+		}
+	}
+}
