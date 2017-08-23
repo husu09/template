a49c088e52bd0c2c6c518faa07c3cdf0dfcad2c9
husu <husu@DESKTOP-PED9DRO>
2017-08-24 00:16:58 +0800

    t

--- src/main/java/com/su/netty/base/ServerHandler.java
+++ src/main/java/com/su/netty/base/ServerHandler.java
@@ -10,6 +10,27 @@ public class ServerHandler extends SimpleChannelInboundHandler<String> {
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
 		System.out.println(msg);
 	}
+	
+	//客户端连接
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelActive");
+		super.channelActive(ctx);
+	}
+	
+	//客户端断开连接
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelInactive");
+		super.channelInactive(ctx);
+	}
+	
+	//异常
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("exceptionCaught");
+		super.exceptionCaught(ctx, cause);
+	}
 
 	
 	
