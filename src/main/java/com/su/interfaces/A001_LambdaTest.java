16af4a61df35a59d795cd112c8e573bc5a49d8f4
husu09 <husu09@qq.com>
2017-10-19 16:01:16 +0800

    t

---
+++ src/main/java/com/su/interfaces/A001_LambdaTest.java
@@ -0,0 +1,32 @@
+package com.su.interfaces;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
+/**
+ * lambda 表达式测试
+ * 
+ * @author husu 2017/10/19
+ */
+public class A001_LambdaTest {
+	public static void main(String[] args) {
+		String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",
+				"Neptune" };
+		System.out.println(Arrays.toString(planets));
+		System.out.println("Sorted in dictionary order:");
+		Arrays.sort(planets);
+		System.out.println(Arrays.toString(planets));
+		System.out.println("Sorted by length:");
+		Arrays.sort(planets, (first, second) -> first.length() - second.length());
+		System.out.println(Arrays.toString(planets));
+
+		Timer t = new Timer(1000, event -> System.out.println("The time is " + new Date()));
+		t.start();
+		
+		JOptionPane.showMessageDialog(null, "Quit program?");
+		System.exit(0);
+	}
+}
